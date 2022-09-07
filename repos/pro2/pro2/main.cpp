870ac5fff18bffbc75bbfc34c4dbd254338af235
KELLO83 <skyblizzard64@gmail.com>
2022-09-07 16:08:36 +0900

    test

--- repos/pro2/pro2/main.cpp
+++ repos/pro2/pro2/main.cpp
@@ -1,41 +1,29 @@
 #include <iostream>
-
-using namespace std;
-int get_average(int,int);
 char* get_grade(int);
+
 int main() {
-	int arr[2][2] = {0};
-	int score_average_buffer[2];
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < 2; j++) {
-			cin >> arr[i][j];
-		}
-	}
-	for (int t = 0; t < 2; t++) {
-		for (int j = 0; j< 2; j++) {
-			score_average_buffer[t] = get_average(arr[t][0], arr[t][1]);
-		}
-	}
-	for (int k = 0; k < 2; k++) {
-		get_grade(score_average_buffer[k]);
-	}
+	int data[2];
 	char* res;
-}
-int get_average(int d1,int d2) {
-	return (d1 + d2) / 2;
+	std::cin >> data[0] >> data[1];
 
+	for (int i = 0; i < 2; i++) {
+		res = get_grade(data[i]);
+		std::cout << res<<std::endl;
 
+	}
 }
 char* get_grade(int data) {
-	char *string = (char*)malloc(sizeof(char) * 20);
-	if(data>90){
-		string = (char*)"A+";
+	char* string = (char*)malloc(sizeof(char*) * 20);
+	if (data > 50) {
+		string = (char*)"PASS";
+
 		return string;
 	}
 	else
 	{
-		string = (char*)"P";
+		string = (char*)"NP";
+	
 		return string;
+
 	}
 }
