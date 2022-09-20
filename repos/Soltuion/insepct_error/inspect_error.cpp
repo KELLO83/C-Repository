680c56fd0cd5dd82e5f8dedf79ed81c10d16b810
KELLO83 <skyblizzard64@gmail.com>
2022-09-20 13:12:59 +0900

    공백을 입력해도 오류없이 정상작동

---
+++ repos/Soltuion/insepct_error/inspect_error.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstring>
+#include <format>
+#include <cctype>
+using namespace std;
+int main() {
+	string str;
+	string user_id;
+	string user_pass;
+	string pass_user_id = "scott";
+	string pass_user_pass = "tiger";
+	string user_input;
+	while (true)
+	{
+		cout << format("id를입력해주세요");
+		getline(cin, user_id);
+		if (user_id ==" ") {
+			break;
+		}
+		if (user_id.empty() == true) {
+			break;
+		}
+		if (user_id == pass_user_id) {
+			cout << format("password를 입력해주세요");
+			getline(cin, user_pass);
+			if (user_pass == " ") {
+				break;
+			}
+			if (user_pass.empty() == true) {
+				break;
+			}
+			if (user_pass == pass_user_pass) {
+				while (true)
+				{
+
+
+					cout << format("SQL->");
+					getline(cin, user_input);
+					if (user_input == " ") {
+						break;
+					}
+					if (user_input.empty() == true) {
+						break;
+					}
+					else
+					{
+						cout << user_input << endl;
+					}
+				}
+				break;
+			}
+			else
+			{
+				cout << format("password가 불일치합니다") << endl;
+				continue;
+			}
+		}
+		else
+		{
+			cout << format("해당 ID {}가 없습니다", user_id) << endl;
+
+		}
+
+
+	}
+	cout <<endl<< "프로그램을 종료합니다";
+}
