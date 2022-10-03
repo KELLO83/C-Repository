0eecd10882a4421f0340c89bf4317f2febdbad97
KELLO83 <skyblizzard64@gmail.com>
2022-10-03 21:35:18 +0900

    위임/타겟 생성자 초기화

---
+++ repos/C++ED/C++ED/Constructor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <format>
+using namespace std;
+class Point {
+	int x, y;
+public:
+	Point();
+	Point(int a, int b);
+	void show() {
+		cout << format("x= {} y= {}\n", x, y);
+	}
+};
+
+Point::Point() :Point(0, 0) {} //위임생성자
+Point::Point(int a, int b)//타겟 생성자
+	:x(a) ,y(b) {}// x=a y=b
+
+int main() {
+	Point c1;
+	Point c2(5, 3);
+	c1.show();
+	c2.show();
+}
